43078d558f1c664b8248d16a6587a95c9e85a9bd
Aleksandr Gursky <maveriks.echo@gmail.com>
2019-08-29 11:14:50 +0300

    init commit

--- README.md
+++ README.md
@@ -12,18 +12,5 @@ npm run serve
 
 ### Compiles and minifies for production
 ```
-npm run build
-```
+npm run build```
 
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div id="chartdiv" ref="chartdiv">
+        </div>
+        <select class="selected" v-model="select" @change="changeData">
+            <option>first</option>
+            <option>second</option>
+        </select>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import * as am4core from "@amcharts/amcharts4/core";
+    import * as am4charts from "@amcharts/amcharts4/charts";
+    import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    am4core.useTheme(am4themes_animated);
+    export default {
+        name: 'chart',
+        data: () => ({
+            chart: null,
+            select: 'first',
+            data:[],
+        }),
+        mounted() {
+            this.getData();
+        },
+        methods:{
+            draw(data){
+                this.chart = am4core.create(this.$refs.chartdiv, am4charts.RadarChart);
+                this.chart.data = data;
+
+                /* Create axes */
+                let categoryAxis = this.chart.xAxes.push(new am4charts.CategoryAxis());
+                categoryAxis.dataFields.category = "label";
+
+
+                let valueAxis = this.chart.yAxes.push(new am4charts.ValueAxis());
+                valueAxis.renderer.axisFills.template.fill = this.chart.colors.getIndex(2);
+                valueAxis.renderer.axisFills.template.fillOpacity = 0.05;
+                valueAxis.renderer.gridType = 'polygons';
+                valueAxis.renderer.labels.template.disabled = true;
+                valueAxis.min = 10;
+                valueAxis.max = 50;
+
+                /* Create and configure series */
+                let series = this.chart.series.push(new am4charts.RadarSeries());
+                series.dataFields.valueY = "value";
+                series.dataFields.categoryX = "label";
+                series.strokeWidth = 3;
+                series.tooltip.autoTextColor = false;
+                series.tooltip.label.fill = am4core.color("black");
+                series.fill = am4core.color("#ccc");
+
+                let circleBullet = series.bullets.push(new am4charts.CircleBullet());
+                circleBullet.circle.fill = am4core.color("#fff");
+                circleBullet.circle.strokeWidth = 3;
+                circleBullet.tooltipText = "[bold]{label}[/] \n {value} points[/]";
+
+
+                this.chart.cursor = new am4charts.RadarCursor();
+            },
+            changeData(){
+                this.chart.dispose();
+                return this.getData();
+            },
+            getData(){
+                let url = './data.json';
+                if (this.select === 'second'){
+                    url = './data2.json'
+                }
+                this.axios.get(url).then(response => {
+                    console.log(response);
+                    this.data = response.data;
+                    this.draw(this.data);
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+        },
+        beforeDestroy() {
+            if (this.chart) {
+                this.chart.dispose();
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    }
+
+    #chartdiv {
+        width: 80%;
+        height: 500px;
+        float: left;
+    }
+    .selected{
+        float: right;
+    }
+
 </style>
